0aacc21f1b6a37a737b3393a93500ca818f49d5a
CarlosFavarao <carlosfavarao12@gmail.com>
2025-05-20 11:41:44 -0300

    Adicionei o controller do Ponto de coleta

--- backend/GiraBem/src/main/java/br/com/GiraBem/controller/PontoColetaController.java
+++ backend/GiraBem/src/main/java/br/com/GiraBem/controller/PontoColetaController.java
@@ -1,4 +1,58 @@
 package br.com.GiraBem.controller;
 
+import br.com.GiraBem.model.CidadeModel;
+import br.com.GiraBem.model.PontoColetaModel;
+import br.com.GiraBem.service.PontoColetaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pontos-coleta")
 public class PontoColetaController {
+    @Autowired
+    private PontoColetaService pontoColetaService;
+
+    //CRUD e Busca Padrão
+    @GetMapping
+    public List<PontoColetaModel> listarTodos(){
+        return pontoColetaService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PontoColetaModel> buscarPorId(@PathVariable Long id){
+        Optional<PontoColetaModel> ponto = pontoColetaService.buscarPontoPorId(id);
+        return ponto.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PontoColetaModel> atualizar(@PathVariable Long id, @RequestBody PontoColetaModel ponto){
+        Optional<PontoColetaModel> atualizado = pontoColetaService.atualizar(id, ponto);
+        return atualizado.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        if (pontoColetaService.deletarPonto(id)){
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //Busca e Endpoints personalizada
+
+    @GetMapping("/filtrar")
+    public List<PontoColetaModel> filtrarPorTipoECidade(@RequestParam Long tipoDoacaoId, @RequestParam Long cidadeId){
+        return pontoColetaService.filtrarPorTipoECidade(tipoDoacaoId, cidadeId);
+    }
+
+    @GetMapping("cidades-disponiveis")
+    public List<CidadeModel> listarCidadesPorTipo(@RequestParam Long tipoDoacaoId){
+        return pontoColetaService.listarCidadesPorTipo(tipoDoacaoId);
+    }
+
 }
